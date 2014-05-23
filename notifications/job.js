373d7b159bc4fccdb8dd69ee8c416e2a49293936
Jayram Kumar <jayram.chandan@gmail.com>
2014-05-23 13:17:11 +0530

    All set to go

---
+++ notifications/job.js
@@ -0,0 +1,111 @@
+var schedule = require('node-schedule');
+var async = require('async');
+var SmsSender = require('./SmsSender');
+var GcmBatcher = require('./gcmBatcher');
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [new schedule.Range(0, 6)];
+rule.hour = 11;
+rule.minute = 36;
+
+var mongoose = require('mongoose');
+var fs = require('fs');
+
+mongoose.connect('mongodb://localhost/twyst');
+
+var Schema = mongoose.Schema;
+var notif = require('../models/notif');
+var Notif = mongoose.model('Notif');
+
+seriesExecuter();
+var j = schedule.scheduleJob(rule, function(){	
+	seriesExecuter();
+});
+
+function seriesExecuter() {
+	async.parallel({
+	    sms: function(callback) {
+	    	smsNotifs(callback);
+	    },
+	    gcm: function(callback) {
+	    	gcmNotifs(callback);
+	    }
+	}, function(err, results) {
+	    console.log(results);
+	});
+
+};
+
+function smsNotifs (callback) {
+	Notif.find({
+		message_type: 'SMS',
+		scheduled_at: {
+			$gte: new Date(Date.now() - 30 * 60 * 1000),
+			$lt: new Date(Date.now() + 30 * 60 * 1000)
+		},
+		status: 'DRAFT'
+	}, function(err, notifs) {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			sendSmsNotifs(notifs);
+		}
+	});
+
+	function sendSmsNotifs (notifs) {
+		var length = notifs.length;
+		if(length === 0) {
+			callback(null, notifs);
+		}
+		else {
+			notifs.forEach(function (item) {
+				item.status = 'SENT';
+				item.save();
+				if(item.phones && item.phones.length > 0) {
+					item.phones.forEach(function (phone) {
+						SmsSender.sendSms(phone, item.body);
+					});
+				}
+				if (--length === 0) {
+					callback(null, notifs);
+				};
+			});
+		}
+	}
+}
+
+
+function gcmNotifs (callback) {
+	Notif.find({
+		message_type: 'GCM',
+		scheduled_at: {
+			$gte: new Date(Date.now() - 30 * 60 * 1000),
+			$lt: new Date(Date.now() + 30 * 60 * 1000)
+		},
+		status: 'DRAFT'
+	}, function(err, notifs) {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			sendGcmNotifs(notifs);
+		}
+	});
+
+	function sendGcmNotifs (notifs) {
+		var length = notifs.length;
+		if (length === 0) {
+			callback(null, notifs);
+		}
+		else {
+			notifs.forEach(function (item) {
+				item.status = 'SENT';
+				item.save();
+				GcmBatcher.sendPush(item);
+				if (--length === 0) {
+					callback(null, notifs);
+				};
+			});
+		}
+	}
+}
---
+++ notifications/smsSender.js
@@ -0,0 +1,49 @@
+var sms_push_url = "http://myvaluefirst.com/smpp/sendsms?username=twysthttp&password=twystht6&to=";
+var smsMessageSentLogs = require('../models/smsMessageSentLogs');
+var http = require('http');
+http.post = require('http-post');
+var mongoose = require('mongoose');
+var smsSentLog = require('../models/smsMessageSentLogs');
+var SmsSentLog = mongoose.model('SmsSentLog');
+
+module.exports.sendSms = function (phone, push_message) {
+
+	push_message = push_message.replace(/(\n)+/g, '');
+	
+	var message = push_message.replace(/&/g,'%26');
+	message = message.replace(/% /g,'%25 ');
+	console.log(message);
+	saveSentSms(phone, message);
+	var send_sms_url = sms_push_url + phone + "&from=TWYSTR&udh=0&text=" + message;
+	var test_url = 'http://staging.twyst.in/api/v2/sms/status';
+	http.post(send_sms_url, function(res){
+		console.log(res.statusCode);
+		res.on('data', function(chunk) {
+            // append chunk to your data
+            body += chunk;
+        });
+
+        res.on('end', function() {
+            console.log(body);
+        });
+
+        res.on('error', function(e) {
+            console.log("Error message: " + e.message)
+        });
+	});
+}
+
+function saveSentSms (phone, message) {
+
+	var sms_log = {};
+	sms_log.phone = phone;
+	sms_log.message = message;
+	
+	var sms_log = new SmsSentLog(sms_log);
+
+	sms_log.save(function (err) {
+		if(err) {
+			console.log(err);
+		}
+	});
+}
