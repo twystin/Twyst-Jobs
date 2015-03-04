fe05ff9e7ae9593860f5b1d075dba549105884f7
arunr <arunr@outlook.com>
2015-03-04 17:54:07 +0530

    Testing some stuff

--- common/birthdayImporter.js
+++ common/birthdayImporter.js
@@ -11,26 +11,31 @@ csv()
 	.from.stream(fs.createReadStream(__dirname + '/birthdays.csv', { encoding: 'utf8' }))
 	.on('record', function (row, index) {
 		//console.log(row[4], row[7], row[6], row[5], new Date(row[7], row[6]-1, row[5]))
-	    Account.findOne({phone:row[4]}, function(e,a) {
+	    if (index === 1) {
+	    	Account.findOne({phone:row[4]}, function(e,a) {
 	    	if (e) {
 	    		//console.log(e);
 	    	} else {
 	    		if (!a) {
 	    			//console.log("Not found!");
 	    		} else {
+	    			console.log("Saving")
 	    			a.profile.bday.y = row[7];
 	    			a.profile.bday.m = row[6];
 	    			a.profile.bday.d = row[5];
 	    			a.profile.first_name = row[1] || '';
 	    			a.profile.last_name = row[3] || '';
 	    			a.profile.email = row[8] || '';
-	    			a.save(function(err) {
+	    			a.save(function(s, err) {
+	    				console.log(s);
 	    				console.log(err);
 	    			});
 	    		}
 	    		//console.log(a);
 	    	}
 	    })
+	    }
+	    
 	})
 	.on('end', function (count) {
 		//console.log("I am finished.")
