f1fae236a23422256c265a9586e8cf6d63e3f41f
arunr <arunr@outlook.com>
2015-03-04 17:06:19 +0530

    Birthday importer added

---
+++ common/birthdayImporter.js
@@ -0,0 +1,37 @@
+var csv = require('csv');
+var fs = require("fs");
+var account = require('../models/account.js');
+
+var mongoose = require('mongoose');
+var Account = mongoose.model('Account');
+
+mongoose.connect('mongodb://localhost/twyst');
+
+csv()
+	.from.stream(fs.createReadStream(__dirname + '/birthdays.csv', { encoding: 'utf8' }))
+	.on('record', function (row, index) {
+		//console.log(row[4], row[7], row[6], row[5], new Date(row[7], row[6]-1, row[5]))
+	    Account.findOne({phone:row[4]}, function(e,a) {
+	    	if (e) {
+	    		//console.log(e);
+	    	} else {
+	    		if (!a) {
+	    			//console.log("Not found!");
+	    		} else {
+	    			a.profile.bday.y = row[7];
+	    			a.profile.bday.m = row[6];
+	    			a.profile.bday.d = row[5];
+	    			a.profile.first_name = row[1] || '';
+	    			a.profile.last_name = row[3] || '';
+	    			a.profile.email = row[8] || '';
+	    			a.save(function(err) {
+	    				console.log(err);
+	    			});
+	    		}
+	    		//console.log(a);
+	    	}
+	    })
+	})
+	.on('end', function (count) {
+		//console.log("I am finished.")
+	})
