cfbaaa7beff2babdb34d1041451554aa72a068d3
arunr <arunr@outlook.com>
2015-03-11 22:51:17 +0530

    Cleaned up birthday job
    Cleaned up birthday job code, and also moved the birthday import code
    into the same director.

--- config/config_models.js
+++ config/config_models.js
@@ -8,8 +8,8 @@ module.exports = function () {
         'account', 'offer', 'outlet', 'program', 'voucher', 'tier', 'recommendation',
         'checkin', 'favourite', 'qr_code', 'tag', 'beta_users', 'log', 'smslog',
         'smsMessageSentLogs', 'redirect', 'temp_otp', 'notif', 'social', 'rating',
-        'recco_config', 'userLoc', 'rewards', 'winback', 'beta_merchants', 'group_program', 
-        'feedback', 'unsbs', 'voucher_reminder'
+        'recco_config', 'userLoc', 'rewards', 'winback', 'beta_merchants', 'group_program',
+        'feedback', 'unsbs', 'voucher_reminder', 'specialProgram'
     ];
 
     models.forEach(function (m) {
